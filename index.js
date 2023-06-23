ca3216694d775cbb6eb5bd46fc03059fc002d3e8
Little-F1sh <itsnihon1@gmail.com>
2023-06-24 02:12:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+// ------class-16------
+
+
+// var rahim = 200;
+// var karim = 300;
+
+// console.log(rahim <= karim);
+
+// -----While Loop-----
+
+// var startFrom = 0;
+
+// while(startFrom < 6) {
+//     console.log("I have started");
+//     // startFrom = startFrom + 1;
+//     // startFrom += 1;
+//     startFrom ++;
+// }
+
+// var num = 0;
+
+// while (num < 200) {
+//     console.log("")
+//     num ++;
+//     console.log(num);
+// }
+
+// var num = 2;
+
+// while(num <= 20) {
+//     console.log(num);
+//     num = num + 2;
+//  }
+
+// var num = 1;
+
+// while(num <= 20) {
+//     console.log(num);
+//     num = num + 2;
+// }
+
+//----- For Loop -----
+
+// var num = 1;
+// for (num =1; num <=20;num = num + 2){
+//     console.log(num);
+// }
+
+// var rostGiven =0;
+// while(rostGiven <= 100) {
+//     console.log("ame rost payace");
+//     rostGiven++;
+//     console.log(rostGiven);
+// }
+
+// var rostGiven = 0;
+// for (rostGiven = 1; rostGiven <= 100; rostGiven++) {
+//     console.log("ame rost payace");
+//     console.log(rostGiven);
+// }
+
+// for(var index = 1; index <= 50; index += 2) {
+//     console.log(index);
+// }
+
+// for (var num1 = 1; num1 <= 20; num1 += 2) {
+//     console.log(num1);
+// }
+
+var myArray = [ 20,30,40,50,60];
+
+// console.log(myArray.length);
+// var index = myArray.indexOf(70);
+// console.log(index);
+// myArray.push(70);
+// myArray.pop();
+// myArray.unshift(80);
+// myArray.shift();
+// console.log(myArray);
+
+// for (var i = 0; i < myArray.length; i++) {
+//     var element = myArray[i];
+//     console.log(element);
+// }
+
+var listOfName = ['a', 'b', 'c', 'd', 'e'];
+
+// for(var i = 0; i <listOfName.length; i++) {
+//     var element =listOfName[i];
+//     console.log(element);
+// }
+
